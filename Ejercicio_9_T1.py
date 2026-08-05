e9757c41d60f347484bb8eb7ac20fe896170e962
ederhoppe <eder.creaper@gmail.com>
2026-08-04 19:13:59 -0600

    V1: Script nuevo
    Versión original

---
+++ Ejercicio_9_T1.py
@@ -0,0 +1,6 @@
+"""9. Escribe un programa que te permita ingresar una cadena de texto y 
+ a salida sea la misma cadena pero con todas las letras en mayúsculas"""
+
+print('Bienvenido al trueque de mayusculas')
+string1 = input('¿Que deasea convertir a mayusculas? ')
+print(f'Resultado: {string1.upper()}')
